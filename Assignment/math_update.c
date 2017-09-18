ef28de6f5ad4ef92a02308430785f44081abc317
Brandon <salamoneb0@students.rowan.edu>
2017-09-18 15:16:10 -0400

    Updated C File Documentation

--- Assignment/math_update.c
+++ Assignment/math_update.c
@@ -6,15 +6,18 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
+#include <math.h>
 
 int math(int num1, int num2,char Operator);
 
 int main()
 {
+    // Variables used in math function
     int num1;
     int num2;
     char Operator;
     
+    // Prompt asking for two numbers and the operator.
     printf("Type the First Number: ");
     scanf ("%d", &num1);
     
@@ -24,11 +27,15 @@ int main()
     printf("Type the Second Number: ");
     scanf("%d", &num2);
     
+    // Function Call and Read-Back
     printf("The Answer Is: %d", math(num1,num2,Operator));
 
     return 0;
 }
 
+/* Primary Function is a series of if and else if statements
+that perform different operations determined by user input
+*/
 int math(int num1, int num2,char Operator)
 {
     if(Operator == '+')
